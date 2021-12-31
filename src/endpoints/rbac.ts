463eb07d7946af9394e4aabf8f9d4a92251942c0
Cale Teeter <cale.teeter@microsoft.com>
2021-12-31 13:30:31 -0500

    Adding creation of role assignments

--- src/endpoints/rbac.ts
+++ src/endpoints/rbac.ts
@@ -1,6 +1,6 @@
 
 import * as ccfapp from "@microsoft/ccf-app";
-import { addRole, getRole, removeRole, updateRole } from "@windozer/ccf-identity";
+import { addRole, assignPrincipalToRole, getRole, removeRole, updateRole } from "@windozer/ccf-identity";
 import { IdentityResponse } from "@windozer/ccf-identity/dist/models/identityResponse";
 import { IRoleDefinition } from "@windozer/ccf-identity/dist/models/roleDefinition";
 
@@ -23,5 +23,11 @@ export function removeRbacRole(request: ccfapp.Request): ccfapp.Response<Identit
 
 export function updateRbacRole(request: ccfapp.Request<IRoleDefinition>): ccfapp.Response<IdentityResponse> {
     const result = updateRole(request.body.json());
+    return { body: result };
+}
+
+export function assignPrincipalToRbacRole(request: ccfapp.Request): ccfapp.Response<IdentityResponse> {
+    const requestBody = request.body.json();
+    let result = assignPrincipalToRole(requestBody.roleId, requestBody.principalId);
     return { body: result };
 }

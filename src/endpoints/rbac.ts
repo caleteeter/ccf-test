2d5d30146a8e2be8365c8870d7b2a45fd3bbd936
Cale Teeter <cale.teeter@microsoft.com>
2021-12-31 13:16:46 -0500

    Added read and delete for roles

--- src/endpoints/rbac.ts
+++ src/endpoints/rbac.ts
@@ -1,9 +1,22 @@
 
 import * as ccfapp from "@microsoft/ccf-app";
-import { addRole } from "@windozer/ccf-identity";
+import { addRole, getRole, removeRole } from "@windozer/ccf-identity";
+import { IdentityResponse } from "@windozer/ccf-identity/dist/models/identityResponse";
 import { IRoleDefinition } from "@windozer/ccf-identity/dist/models/roleDefinition";
 
-export function addRbacRole(request: ccfapp.Request<IRoleDefinition>): ccfapp.Response {
-    let result = addRole(request.body.json());
+export function addRbacRole(request: ccfapp.Request<IRoleDefinition>): ccfapp.Response<IdentityResponse> {
+    const result = addRole(request.body.json());
+    return { body: result };
+}
+
+export function getRbacRole(request: ccfapp.Request): ccfapp.Response<IdentityResponse | IRoleDefinition> {
+    const id = parseInt(request.query.split('=')[1]);
+    const result = getRole(id.toString());
+    return { body: result };
+}
+
+export function removeRbacRole(request: ccfapp.Request): ccfapp.Response<IdentityResponse> {
+    const id = parseInt(request.query.split('=')[1]);
+    const result = removeRole(id.toString());
     return { body: result };
 }
